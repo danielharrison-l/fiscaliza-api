ce1153793cceffaef059fa39b4068d645961acec
harrison <d.harrison.lima@gmail.com>
2025-05-16 07:58:08 -0300

    feat: adiciona .env e .env.example

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 /// <reference types="node" />
+import { config } from 'dotenv';
 import { buildApp } from './app';
+
+// Carrega as variáveis de ambiente
+config();
+
+const PORT = process.env.PORT || 3000;
 
 const start = async () => {
   try {
     const app = await buildApp();
-    await app.listen({ port: 3000, host: '0.0.0.0' });
-    console.log('Servidor rodando em http://localhost:3000');
+    await app.listen({ port: Number(PORT), host: '0.0.0.0' });
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
   } catch (err) {
     console.error(err);
     process.exit(1);
